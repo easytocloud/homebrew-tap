5ccf65978e8ab95039bcd6a8ffb8c01fc114c442
Erik Meinders <erik@easytocloud.com>
2022-11-13 20:12:11 +0100

    Update privpage to V0.1.2

--- Formula/privpage.rb
+++ Formula/privpage.rb
@@ -2,9 +2,9 @@ class Privpage < Formula
   desc "AWS Privacy Pager"
   homepage "https://github.com/easytocloud/privpage.git"
   license "MIT"
-  url "https://github.com/easytocloud/privpage/archive/refs/tags/v0.1.1.zip"
-  sha256 "5761bec378abe9cade48de0c8832f1b06f9c13089157d59252454fbde4e7990c"
-  version '0.1.1'
+  url "https://github.com/easytocloud/privpage/archive/refs/tags/v0.1.2.zip"
+  sha256 "f3f374ee6286eaf0a6c97dc799ec2d72edb7fe1f638c606a080e9f852ac6723b"
+  version '0.1.2'
 
   def install
 
