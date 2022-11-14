44200559e3911912a4e1f0e3d0ca5665033ea8fb
Erik Meinders <erik@easytocloud.com>
2022-11-14 13:12:16 +0100

    Added sha256 for for-tools

--- Formula/for-tools.rb
+++ Formula/for-tools.rb
@@ -3,6 +3,7 @@ class ForTools < Formula
   homepage "https://github.com/easytocloud/for-tools.git"
   license "MIT"
   url "https://github.com/easytocloud/for-tools/archive/refs/tags/v0.1.0.zip"
+  sha256 "ed6d91c8004def67ac7f41b8a18b89a623108f9258c7692756ed5b402fbb8271"
   version '0.1.0'
 
   def install
