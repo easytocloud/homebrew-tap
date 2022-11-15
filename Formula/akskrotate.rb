9778aedd2539edf505939acad3f54e65a5bdcae6
Erik Meinders <erik@easytocloud.com>
2022-11-15 11:06:01 +0100

    Added checksum for akskrotate

--- Formula/akskrotate.rb
+++ Formula/akskrotate.rb
@@ -3,6 +3,7 @@ class Akskrotate < Formula
   homepage "https://github.com/easytocloud/for-tools.git"
   license "MIT"
   url "https://github.com/easytocloud/akskrotate/archive/refs/tags/v0.1.0.zip"
+  sha256 "2ea1872aa86cb7c6741c6aa9905cb5341f777fd1683aef95e88a6c05fa576ab2"
   version '0.1.0'
 
   def install
