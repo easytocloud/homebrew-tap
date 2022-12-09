37cd38513e75456229c3df4f140ad3f749bcee96
homebrew-releaser <homebrew-releaser@easytocloud.com>
2022-12-09 07:49:53 +0000

    "Brew formula update for akskrotate version v0.2.0"

--- Formula/akskrotate.rb
+++ Formula/akskrotate.rb
@@ -5,8 +5,8 @@
 class Akskrotate < Formula
   desc "Rotate your aws access key / secret key with confidence"
   homepage "https://github.com/easytocloud/akskrotate"
-  url "https://github.com/easytocloud/akskrotate/archive/v0.1.3.tar.gz"
-  sha256 "e53bc00b0982b3cbf62cd5c22a15f549313ff2bf891b466b83f135b0264d5638"
+  url "https://github.com/easytocloud/akskrotate/archive/v0.2.0.tar.gz"
+  sha256 "564d562dfc8f00cb92cd81ca5e804f1537972137413524b56f6d18bb180c5f5a"
   license "MIT"
 
   depends_on "bash" => :build
