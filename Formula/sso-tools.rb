63c362c6736437f72a3e2f39bea54e337a9a33e4
homebrew-releaser <homebrew-releaser@easytocloud.com>
2024-05-02 08:59:16 +0000

    "Brew formula update for sso-tools version v0.1.6"

--- Formula/sso-tools.rb
+++ Formula/sso-tools.rb
@@ -5,12 +5,13 @@
 class SsoTools < Formula
   desc "Sso tools for use in aws ec2 instances"
   homepage "https://github.com/easytocloud/sso-tools"
-  url "https://github.com/easytocloud/sso-tools/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "845e491dc92773ce941f10c1e65bd67f6f9b5eabd80da9bc1bce95a9594432b1"
+  url "https://github.com/easytocloud/sso-tools/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "7993cde91c0f3918353256b628152d856e0d26948bf288230f7e3bd6b057f2ec"
 
   def install
     bin.install "distribution/bin/ssostart"
 bin.install "distribution/bin/cclist"
+bin.install "distribution/bin/ccinit"
 bin.install "distribution/bin/ccclone"
 bin.install "distribution/bin/generate-sso-config"
   end
