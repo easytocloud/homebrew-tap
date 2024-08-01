07d878e8bc8a618389d524a00535875f2396198c
homebrew-releaser <homebrew-releaser@easytocloud.com>
2024-08-01 09:36:43 +0000

    chore: brew formula update for for-tools v0.2.9

--- Formula/for-tools.rb
+++ Formula/for-tools.rb
@@ -5,8 +5,8 @@
 class ForTools < Formula
   desc "Aws cli tools to iterate over multiple profiles, roles, users or aws organizatio"
   homepage "https://github.com/easytocloud/for-tools"
-  url "https://github.com/easytocloud/for-tools/archive/refs/tags/v0.2.8.tar.gz"
-  sha256 "20402f13da051818c20d885fb2e2d1855e3bd012f05edd443940bf2e978ce6b9"
+  url "https://github.com/easytocloud/for-tools/archive/refs/tags/v0.2.9.tar.gz"
+  sha256 "7f526de56658e8e9cf4a3d68e4c1f73c0aab8cd237c5efb2b475dff7f885446f"
   license "MIT"
 
   depends_on "bash" => :build
