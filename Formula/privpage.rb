66c3ab13ecc158d686cfe41d8946e8769a1b3d37
homebrew-releaser <homebrew-releaser@easytocloud.com>
2024-08-15 08:59:45 +0000

    chore: brew formula update for privpage v0.4.2

--- Formula/privpage.rb
+++ Formula/privpage.rb
@@ -5,8 +5,8 @@
 class Privpage < Formula
   desc "Aws pager masking privacy sensitive information"
   homepage "https://github.com/easytocloud/privpage"
-  url "https://github.com/easytocloud/privpage/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "ac2eb5d84a9aa484ff44d62dd279f224d650ebda924dbaa6dd7ad0016b55dcd7"
+  url "https://github.com/easytocloud/privpage/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "2c147a64072baeae3484cc5724ca497cef1d077fa4599bbf0d1f010328813a70"
   license "MIT"
 
   def install
