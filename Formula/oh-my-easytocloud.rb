bc8a2e6431979b80d6157807dddd38fa38eb27e4
homebrew-releaser <homebrew-releaser@easytocloud.com>
2024-09-29 15:43:02 +0000

    chore: brew formula update for oh-my-easytocloud v0.2.2

--- Formula/oh-my-easytocloud.rb
+++ Formula/oh-my-easytocloud.rb
@@ -5,19 +5,21 @@
 class OhMyEasytocloud < Formula
   desc "Upgrade oh-my-zsh agnoster theme with aws_env in prompt"
   homepage "https://github.com/easytocloud/oh-my-easytocloud"
-  url "https://github.com/easytocloud/oh-my-easytocloud/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "ca517202fba0e32dd28b4b51e6d335845eb5ee5ff2a4e04c181c568b05580db1"
+  url "https://github.com/easytocloud/oh-my-easytocloud/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "0073cea37cb1bbeb1315e3764c3a31e5f18dcd58a2b6ec7f82cfbd1015f91893"
   license "MIT"
 
   def install
-    # Install the plugin directory to ~/.oh-my-zsh/custom/plugins
-plugin_dir = File.expand_path("~/.oh-my-zsh/custom/plugins/easytocloud")
-mkdir_p plugin_dir
-cp_r "plugins/easytocloud/.", plugin_dir
+    cd "easytocloud-#{version}" do
+  # Install the plugin directory to ~/.oh-my-zsh/custom/plugins
+  plugin_dir = File.expand_path("~/.oh-my-zsh/custom/plugins/easytocloud")
+  mkdir_p plugin_dir
+  cp_r "plugins/easytocloud/.", plugin_dir
 
-# Install the theme file to ~/.oh-my-zsh/custom/themes
-theme_dir = File.expand_path("~/.oh-my-zsh/custom/themes")
-mkdir_p theme_dir
-cp "themes/easytocloud.zsh-theme", theme_dir
+  # Install the theme file to ~/.oh-my-zsh/custom/themes
+  theme_dir = File.expand_path("~/.oh-my-zsh/custom/themes")
+  mkdir_p theme_dir
+  cp "themes/easytocloud.zsh-theme", theme_dir
+end
   end
 end
