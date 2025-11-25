9f971ff3d50ce2d7de1954253f446c21f6a17006
homebrew-releaser <homebrew-releaser@easytocloud.com>
2025-11-25 08:57:29 +0000

    chore: update oh-my-easytocloud to v0.2.19

--- Formula/oh-my-easytocloud.rb
+++ Formula/oh-my-easytocloud.rb
@@ -4,29 +4,20 @@
 class OhMyEasytocloud < Formula
   desc "Upgrade oh-my-zsh agnoster theme with aws_env in prompt"
   homepage "https://github.com/easytocloud/oh-my-easytocloud"
-  url "https://github.com/easytocloud/oh-my-easytocloud/archive/refs/tags/v0.2.18.tar.gz"
-  sha256 "3f24577d7196ba96c9c6ba2a5db689adf33ff877039a01f10c351ee4d968ee02"
+  url "https://github.com/easytocloud/oh-my-easytocloud/archive/refs/tags/v0.2.19.tar.gz"
+  sha256 "af82913d74ee4e4601950d03687677406efd3c61ee0692cf1507fa2569ba2e80"
   license "MIT"
 
   def install
-    (share/"oh-my-easytocloud/plugins/easytocloud").install Dir["plugins/easytocloud/*"]
-    (share/"oh-my-easytocloud/themes").install "themes/easytocloud.zsh-theme"
+    ohmyzsh = ENV["HOME"] + "/.oh-my-zsh"
+
+    system "mkdir", "-p", ohmyzsh + "/custom/plugins/easytocloud"
+    system "cp", "-R", "plugins/easytocloud/.", ohmyzsh + "/custom/plugins/easytocloud/"
+
+    system "mkdir", "-p", ohmyzsh + "/custom/themes"
+    system "cp", "themes/easytocloud.zsh-theme", ohmyzsh + "/custom/themes/"
+
     (share/"doc/oh-my-easytocloud").install "README.md"
-  end
-
-  def post_install
-    ohmyzsh = Pathname.new(ENV["HOME"]) / ".oh-my-zsh"
-    return unless ohmyzsh.directory?
-
-    # Install plugin
-    plugin_dir = ohmyzsh / "custom/plugins/easytocloud"
-    plugin_dir.mkpath
-    cp_r (share/"oh-my-easytocloud/plugins/easytocloud").children, plugin_dir, remove_destination: true
-
-    # Install theme
-    theme_dir = ohmyzsh / "custom/themes"
-    theme_dir.mkpath
-    cp share/"oh-my-easytocloud/themes/easytocloud.zsh-theme", theme_dir/"easytocloud.zsh-theme", remove_destination: true
   end
 
   def caveats
@@ -40,7 +31,7 @@ class OhMyEasytocloud < Formula
   end
 
   test do
-    assert_predicate share/"oh-my-easytocloud/plugins/easytocloud", :directory?
-    assert_predicate share/"oh-my-easytocloud/themes/easytocloud.zsh-theme", :file?
+    system "test", "-f", ENV["HOME"] + "/.oh-my-zsh/custom/plugins/easytocloud/easytocloud.plugin.zsh"
+    system "test", "-f", ENV["HOME"] + "/.oh-my-zsh/custom/themes/easytocloud.zsh-theme"
   end
 end
