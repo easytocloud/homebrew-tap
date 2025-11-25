57236f1397b193c83df574c0f12e92191ca47e14
homebrew-releaser <homebrew-releaser@easytocloud.com>
2025-11-25 09:01:29 +0000

    chore: update oh-my-easytocloud to v0.2.20

--- Formula/oh-my-easytocloud.rb
+++ Formula/oh-my-easytocloud.rb
@@ -4,20 +4,27 @@
 class OhMyEasytocloud < Formula
   desc "Upgrade oh-my-zsh agnoster theme with aws_env in prompt"
   homepage "https://github.com/easytocloud/oh-my-easytocloud"
-  url "https://github.com/easytocloud/oh-my-easytocloud/archive/refs/tags/v0.2.19.tar.gz"
-  sha256 "af82913d74ee4e4601950d03687677406efd3c61ee0692cf1507fa2569ba2e80"
+  url "https://github.com/easytocloud/oh-my-easytocloud/archive/refs/tags/v0.2.20.tar.gz"
+  sha256 "38d670826e78f063f33a01e3929cbafab0b2f2c6d2388eb7157fb13fea71db02"
   license "MIT"
 
   def install
+    # Install to Homebrew's share directory (always works)
+    (share/"oh-my-easytocloud/plugins/easytocloud").install Dir["plugins/easytocloud/*"]
+    (share/"oh-my-easytocloud/themes").install "themes/easytocloud.zsh-theme"
+    (share/"doc/oh-my-easytocloud").install "README.md"
+  end
+
+  def post_install
+    # post_install runs outside sandbox, so HOME is the real home directory
     ohmyzsh = ENV["HOME"] + "/.oh-my-zsh"
+    return unless Dir.exist?(ohmyzsh)
 
     system "mkdir", "-p", ohmyzsh + "/custom/plugins/easytocloud"
-    system "cp", "-R", "plugins/easytocloud/.", ohmyzsh + "/custom/plugins/easytocloud/"
+    system "cp", "-R", (share/"oh-my-easytocloud/plugins/easytocloud").to_s + "/.", ohmyzsh + "/custom/plugins/easytocloud/"
 
     system "mkdir", "-p", ohmyzsh + "/custom/themes"
-    system "cp", "themes/easytocloud.zsh-theme", ohmyzsh + "/custom/themes/"
-
-    (share/"doc/oh-my-easytocloud").install "README.md"
+    system "cp", (share/"oh-my-easytocloud/themes/easytocloud.zsh-theme").to_s, ohmyzsh + "/custom/themes/"
   end
 
   def caveats
@@ -31,7 +38,7 @@ class OhMyEasytocloud < Formula
   end
 
   test do
-    system "test", "-f", ENV["HOME"] + "/.oh-my-zsh/custom/plugins/easytocloud/easytocloud.plugin.zsh"
-    system "test", "-f", ENV["HOME"] + "/.oh-my-zsh/custom/themes/easytocloud.zsh-theme"
+    assert_predicate share/"oh-my-easytocloud/plugins/easytocloud/easytocloud.plugin.zsh", :exist?
+    assert_predicate share/"oh-my-easytocloud/themes/easytocloud.zsh-theme", :exist?
   end
 end
